5e3e312199c9b76bf3436c2cdb0492f627e4a67d
cortezBraigo <dylan@braigo.mx>
2021-08-14 10:36:34 -0500

    models with eloquent

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\post;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post}', function (Post $post) {
+    
+    // $post = Post::find($slug);
+    
+    // return view('post', ['post' => $post]);
+
+    return view('post', [
+        'post' => $post
+    ]);
+
+});
